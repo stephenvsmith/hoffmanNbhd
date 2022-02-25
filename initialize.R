fab2b29ad3babbbfee1b626ff26944253da5ba39
stephenvsmith <stephensmith13424@gmail.com>
2022-02-24 16:54:42 -0800

    Changed one of the file names

--- initialize.R
+++ initialize.R
@@ -13,7 +13,7 @@ scratch_dir <- '/u/scratch/s/stephens/'
 data_gen_file <- paste0(home_dir,'data_gen.R')
 result_dir <- paste0(scratch_dir,'ResultsNbhd-',format(Sys.Date(),"%m-%y"))
 rds_dir <- paste0(home_dir,'Networks/rds')
-source(paste0(home_dir,'hoffmanNbhdArray/helperfunctions.R'))
+source(paste0(home_dir,'hoffmanNbhd/helperfunctions.R'))
 
 set.seed(555)
 
